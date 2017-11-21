7505f4406247115bb0d9c36a270152e9f53e8cb9
ThisTNTSquid <tntsquid@corecrafted.net>
2017-11-21 23:32:09 +0800

    Fixed app not launching

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 const axios = require("axios");
-const fs = require('fs')
-const path = require('path')
-const config=require('../config')
+const fs = require("fs");
+const path = require("path");
+const config = require("../config");
 
 var suc = 0;
 var fai = 0;
-setInterval(main, 1000/config.speed);
+setInterval(main, 1000 / config.speed);
 
 function main() {
   axios
-    .get(
-      config.url
-    )
+    .get(config.url)
     .then(function(response) {
       suc++;
       console.log(suc + "/" + (suc + fai) + " >> Request OK");
@@ -19,13 +17,7 @@ function main() {
     })
     .catch(function(error) {
       fai++;
-      console.log(
-        fai +
-          "/" +
-          (suc + fai) +
-          " >> Error: " +
-          error.code
-      );
+      console.log(fai + "/" + (suc + fai) + " >> Error: " + error.code);
       // process.exit(0)
       // fs.writeFile(path.join(__dirname,"error.json"),JSON.stringify({...error}),function(err){
       //   if (err){
